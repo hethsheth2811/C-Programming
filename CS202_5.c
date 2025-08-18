033b144782d1942c30d3fa480b9839b8c42d0fa5
Het Sheth <hethsheth2811@gmail.com>
2025-08-18 11:38:40 +0530

    Add files via upload

---
+++ CS202_5.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    /*Program to add, subtract, multiply & divide 2 Nos.*/
+    float a,b;
+    printf("enter a:");
+    scanf("%f", &a);
+    printf("enter b:");
+    scanf("%f", &b);
+    printf("Sum=%f\n", a+b);
+    printf("Difference=%f\n", a-b);
+    printf("Multiplication=%f\n", a*b);
+    printf("Division=%f\n", a/b);
+
+
+    return 0;
+}
