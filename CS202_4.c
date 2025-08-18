b365636cf1e815a4afa77dfed3ed46b3373b45f2
Het Sheth <hethsheth2811@gmail.com>
2025-08-18 11:23:27 +0530

    Add files via upload

---
+++ CS202_4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    /*Program to divide 2 Nos.*/
+    int a,b;
+    printf("enter a:");
+    scanf("%d", &a);
+    printf("enter b:");
+    scanf("%d", &b);
+    printf("%d", a/b);
+
+    return 0;
+}
