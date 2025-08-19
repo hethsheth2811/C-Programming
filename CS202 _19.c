93337dd409c54527b4d6a8fc5843be8f21a2fd2c
Het Sheth <hethsheth2811@gmail.com>
2025-08-19 22:30:01 +0530

    Add files via upload

---
+++ CS202 _19.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    /*Program to calculate area of circle*/
+    float radius;
+    printf("Enter radius: ");
+    scanf("%f", &radius);
+    printf("Area of circle:%f\n",3.14*radius*radius);
+
+
+    return 0;
+}
