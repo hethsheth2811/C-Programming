4ecdbcf9bc45b69da8679b8cda1113ad8bf0545b
Het Sheth <hethsheth2811@gmail.com>
2025-08-19 20:08:33 +0530

    Add files via upload

---
+++ CS202 _9.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    /*Program to convert Rs. into dollars*/
+    float Rs;
+    float dollars;
+	printf("Please enter Rs:");
+	scanf("%f", &Rs);
+	printf("%f $\n", dollars=Rs/48);
+
+    return 0;
+}
