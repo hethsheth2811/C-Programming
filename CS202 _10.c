a55b7a12094b638f5332a5ce8376e73a4b54d63e
Het Sheth <hethsheth2811@gmail.com>
2025-08-19 20:18:36 +0530

    Add files via upload

---
+++ CS202 _10.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    /*Program to convert dollars into pound*/
+    float dollars;
+    float pounds;
+	printf("Please enter dollars:");
+	scanf("%f", &dollars);
+	printf("%f pound\n", pounds=dollars*1.458);
+
+    return 0;
+}
