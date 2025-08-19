7bd7177331ea0bb78b6fdc06bcdf62be781bf021
Het Sheth <hethsheth2811@gmail.com>
2025-08-19 22:54:59 +0530

    Add files via upload

---
+++ CS202 _20.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    /*Program to calculate area of triangle*/
+    float base,height;
+    printf("enter base:");
+    scanf("%f", &base);
+    printf("enter height:");
+    scanf("%f", &height);
+    printf("Area of triangle:%f\n",0.5*base*height);
+
+
+    return 0;
+}
