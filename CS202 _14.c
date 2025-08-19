7f8c3d1bdf1ad7e50e7e987a40748af9d628458d
Het Sheth <hethsheth2811@gmail.com>
2025-08-19 22:09:38 +0530

    Add files via upload

---
+++ CS202 _14.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    /*Program to convert celsius into Fahrenheit*/
+    float celsius, fahrenheit;
+    printf("Enter the temperature in Celsius: ");
+    scanf("%f", &celsius);
+    printf("%f\n",fahrenheit=(celsius*1.8)+32);
+    return 0;
+}
