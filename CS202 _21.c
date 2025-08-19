e99496958cb7fe752f650a82d1efe7745d246dba
Het Sheth <hethsheth2811@gmail.com>
2025-08-19 23:16:27 +0530

    Add files via upload

---
+++ CS202 _21.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    /*Program to calculate net salary*/
+    float gross_salary, allowance, deduction, net_salary;
+    printf("Enter the gross salary: ");
+    scanf("%f", &gross_salary);
+    allowance = 0.10 * gross_salary;
+    deduction = 0.03 * gross_salary;
+    net_salary = gross_salary + allowance - deduction;
+    printf("Net Salary: %f\n", net_salary);
+
+    return 0;
+}
