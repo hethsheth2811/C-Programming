e0b1b6e62a0e0d2d2a4f48e6207ce1f4d22a438d
Het Sheth <hethsheth2811@gmail.com>
2025-08-19 20:23:59 +0530

    Add files via upload

---
+++ CS202 _12.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    /*Program to convert kg into grams*/
+    float grams;
+    float kgs;
+	printf("Please enter kgs:");
+	scanf("%f", &kgs);
+	printf("%f grams\n",grams=kgs*1000);
+
+    return 0;
+}
