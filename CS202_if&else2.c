ba27865723d96cfc8181f637dd2ff2742508410e
Het Sheth <hethsheth2811@gmail.com>
2025-08-20 18:14:20 +0530

    Add files via upload

---
+++ CS202_if&else2.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    /*Program to find out largest and smallest of three values*/
+    int a,b,c;
+    int largest,smallest;
+    printf("Enter three integers: ");
+    scanf("%d %d %d", &a, &b, &c);
+    // Find the largest number
+     if (a >= b && a >= c) {
+        largest = a;
+    } else if (b >= a &&b >= c) {
+        largest = b;
+    } else {
+        largest = c;
+    }
+    // Find the smallest number
+    if (a <= b && a<= c) {
+        smallest = a;
+    } else if (b <= a && b <= c) {
+        smallest = b;
+    } else {
+        smallest = c;
+    }
+    printf("The largest number is: %d\n", largest);
+    printf("The smallest number is: %d\n", smallest);
+
+
+    return 0;
+}
