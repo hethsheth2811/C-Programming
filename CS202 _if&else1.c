c801d13b6856c70bdaa7d0f1bd648c4be54b75f2
Het Sheth <hethsheth2811@gmail.com>
2025-08-20 18:08:59 +0530

    Add files via upload

---
+++ CS202 _if&else1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    /*Program to find out largest and smallest of two values*/
+    int a,b;
+    printf("Enter the first number: ");
+    scanf("%d", &a);
+    printf("Enter the second number: ");
+    scanf("%d", &b);
+    if(a>b)
+        printf("The largest number is: %d\n", a);
+        else if (b > a) {
+        printf("The largest number is: %d\n",b);
+    } else {
+        printf("Both numbers are equal.\n");
+    }
+
+
+    return 0;
+}
