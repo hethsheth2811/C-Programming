fb8c197a46da621693ec005e550796d7137261c1
Het Sheth <hethsheth2811@gmail.com>
2025-08-20 19:30:56 +0530

    Add files via upload

---
+++ CS202_24.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    /*Program to swap two values*/
+    int first, second, x;
+    printf("Enter first number: ");
+    scanf("%d", &first);
+    printf("Enter second number: ");
+    scanf("%d", &second);
+    x = first;
+    first = second;
+    second = x;
+    printf("\nAfter swapping, first number = %d\n", first);
+    printf("After swapping, second number = %d\n", second);
+
+    return 0;
+}
