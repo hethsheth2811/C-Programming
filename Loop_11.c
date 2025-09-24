9c0def4feb64563ad4a92cde4db96a6bf722d721
Het Sheth <hethsheth2811@gmail.com>
2025-09-24 23:37:44 +0530

    Add files via upload

---
+++ Loop_11.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+    int main() {
+    char name[50];
+    int i;
+    printf("Enter your name: ");
+    scanf("%s",name);
+    printf("Printing your name 5 times:\n");
+    for (i = 0; i < 5; i++) {
+        printf("%s\n", name);
+    }
+return 0;
+}
+}
---
+++ Loop_12.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+    int main() {
+    char name[50];
+    int i;
+    printf("Enter your name: ");
+    scanf("%s",name);
+    printf("Printing your name n times:\n");
+    for (i = 0; i < n; i++) {
+        printf("%s\n", name);
+    }
+return 0;
+}
+}
